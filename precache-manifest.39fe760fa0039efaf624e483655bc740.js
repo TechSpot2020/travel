43d695e9726290e93b476968d189996b9f4e5e56
Yuliani Yuliani <Yuliani_Yuliani@Dell.com>
2020-07-20 03:14:36 +0800

    Updates

---
+++ precache-manifest.39fe760fa0039efaf624e483655bc740.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd2736c69651aa16f7ba7b3bb1dada0",
+    "url": "/travel/index.html"
+  },
+  {
+    "revision": "f78917849434f52382dc",
+    "url": "/travel/static/css/main.3127bfe7.chunk.css"
+  },
+  {
+    "revision": "aca8c25e097bd8e5585d",
+    "url": "/travel/static/js/2.9591c483.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/travel/static/js/2.9591c483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78917849434f52382dc",
+    "url": "/travel/static/js/main.e19672ab.chunk.js"
+  },
+  {
+    "revision": "5734ad54b26bd5ccfde1",
+    "url": "/travel/static/js/runtime-main.16f54be6.js"
+  },
+  {
+    "revision": "004f7787a482b13187077a92b8a6c5dc",
+    "url": "/travel/static/media/pay.004f7787.png"
+  },
+  {
+    "revision": "2436f715f7e2d24388c70ea96e99b890",
+    "url": "/travel/static/media/silver.2436f715.png"
+  }
+]);
